e843b132818f54dd34d2a17776b88922553edc2c
George Brocklehurst <george.brocklehurst@gmail.com>
2009-09-24 14:34:44 +0100

    Display build data from a real Cruise status report.

--- build-monitor.rb
+++ build-monitor.rb
@@ -1,9 +1,22 @@
 require 'rubygems'
 require 'sinatra'
 require 'erb'
+require 'rexml/document'
 
 set :public, File.dirname(__FILE__) + '/public'
 
 get '/' do
-  erb :index
+  erb :index, :locals => {:builds => builds}
+end
+
+def builds
+  xml = File.read('/Users/georgebrocklehurst/Downloads/XmlStatusReport.aspx')
+  doc, builds = REXML::Document.new(xml), []
+  doc.elements.each('Projects/Project') do |p|
+    builds << {
+      :title => p.attributes["name"],
+      :status => p.attributes["activity"] == "Building" ? "building" : p.attributes["lastBuildStatus"].downcase
+    }
+  end
+  builds
 end

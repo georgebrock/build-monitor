1dce12045107aacc38909ec60358f47b8cfc0a17
Tom Lea <commit@tomlea.co.uk>
2009-09-25 14:56:09 +0100

    Better handling of failed updates.

--- build-monitor.rb
+++ build-monitor.rb
@@ -8,30 +8,52 @@ require 'yaml'
 set :public, File.dirname(__FILE__) + '/public'
 
 get '/' do
-  erb :index, :locals => {:builds => cruise_status}
+  erb :index, :locals => {:builds => CruiseStatus.builds}
 end
 
-def cruise_status
-  builds = {:success => [], :failure => [], :building => []}
-  servers.each do |server|
-    xml = get_xml(server)
-    doc = REXML::Document.new(xml)
-    doc.elements.each('Projects/Project') do |p|
-      status = p.attributes["activity"] == "Building" ? "building" : p.attributes["lastBuildStatus"].downcase
-      builds[status.to_sym] << p.attributes["name"]
+module CruiseStatus
+
+  def builds
+    @last_cruise_status ||= {}
+
+    builds = {:success => [], :failure => [], :building => [], :update_failed => []}
+
+    servers.each do |server|
+      unless doc = timeout_or_nil(30){ get_xml(server) }
+        builds[:update_failed] << server
+        return builds unless doc = @last_cruise_status[server]
+      end
+
+      doc.elements.each('Projects/Project') do |p|
+        status = p.attributes["activity"] == "Building" ? "building" : p.attributes["lastBuildStatus"].downcase
+        builds[status.to_sym] << p.attributes["name"]
+      end
+
+      @last_cruise_status[server] = doc
     end
+
+    builds
   end
-  builds
-end
 
-def get_xml(server)
-  host, port = server.split(':')
-  http_result = Net::HTTP.start(host, port) {|http|
-    http.get('/XmlStatusReport.aspx')
-  }
-  http_result.body
-end
+  def get_xml(server)
+    host, port = server.split(':')
+    http_result = Net::HTTP.start(host, port) {|http|
+      http.get('/XmlStatusReport.aspx')
+    }
+    REXML::Document.new(http_result.body)
+  end
 
-def servers
-  @servers ||= YAML.load(File.read(File.dirname(__FILE__) + '/config/servers.yaml'))
+  def servers
+    @servers ||= YAML.load(File.read(File.dirname(__FILE__) + '/config/servers.yaml'))
+  end
+
+  def timeout_or_nil(value)
+    Timeout::timeout(10) do
+      return yield
+    end
+  rescue Timeout::Error
+    return nil
+  end
+
+  extend self
 end
